d23c52c19e508c3be47bd7c5c92e75b56f9dea37
developer <developer@EPBYMINW4779.minsk.epam.com>
2015-04-01 18:48:08 +0300

    .

--- files/pre_init.py
+++ files/pre_init.py
@@ -12,7 +12,7 @@ class PreInitConfig(object):
         self.docker_url = "unix://var/run/docker.sock"
         self.docker_version = "1.16"
         self.init_script = "/etc/sensu/sensu-init.sh"
-        self.swarm_container = "swarm-agent"
+        self.swarm_container = os.environ.get("SWARM_AGENT_NAME", "swarm-agent")
         self.run()
 
     def run(self):
@@ -26,15 +26,17 @@ class PreInitConfig(object):
 
             if node_ip_port:
                 node_ip, node_port = string.split(node_ip_port[2], ":")
-
-            os.environ["NODE_NAME"] = str(node_ip)
-            os.environ["NODE_IP"] = str(node_ip)
-            os.environ["NODE_PORT"] = str(node_port)
+                os.environ["NODE_NAME"] = str(node_ip)
+                os.environ["NODE_IP"] = str(node_ip)
+                os.environ["NODE_PORT"] = str(node_port)
         except Exception as e:
             syslog.syslog(syslog.LOG_ERR, "Sensu Client Pre-init:check_node Error: " + e.__str__())
 
     def run_service(self):
-        subprocess.call([self.init_script])
+        try:
+            subprocess.call([self.init_script])
+        except Exception as e:
+            syslog.syslog(syslog.LOG_ERR, "Sensu Client Pre-init:run_service Error: " + e.__str__())
 
 
 if __name__ == "__main__":
